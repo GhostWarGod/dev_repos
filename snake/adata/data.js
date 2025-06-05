240e809724e6e6e1337b8788439367dbc149f74e
hjy <545925654@qq.com>
2025-06-05 21:24:53 +0800

    s0

--- snake/adata/data.js
+++ snake/adata/data.js
@@ -1,48 +1,39 @@
-/*
- * @Descripttion:
- * @version:
- * @Author: hjy
- * @Date: 2025-05-29 11:25:02
- * @LastEditors: hjy
- * @LastEditTime: 2025-06-05 16:54:50
- */
-window.v_type = 1; //0=QA；1=S0; 2=SN;
+window.v_type = 1;
+
 
 window.server1 = "https://sso.queengame.io"; //测试环境
 window.wsServer1 = "wss://wss-hall.queengame.io"; //测试环境
 
+
 var nodeData = [
     {
-        id: 1001,
-        socketUrl: "wss://wss-room-sg.queengame.io",
-        serverUrl: "wss://wss-hall.queengame.io",
-        open: 1,
         area: 1010,
+        open: 1,
+        point: "1_00",
+        id: 1001,
         name: 1010,
+        socketUrl: "wss://wss-room-sg.queengame.io",
         url: "https://wss-room-sg.queengame.io/ping",
-        type: 1,
-        point: "1_00"
+        type: 1
     },
     {
-        id: 1002,
-        socketUrl: "wss://wss-room-us.queengame.io",
-        serverUrl: "wss://wss-hall.queengame.io",
-        open: 0,
         area: 1016,
+        open: 0,
+        point: "2_00",
+        id: 1002,
         name: 1016,
+        socketUrl: "wss://wss-room-us.queengame.io",
         url: "https://wss-room-us.queengame.io/ping",
-        type: 1,
-        point: "2_00"
+        type: 1
     },
     {
-        id: 1003,
-        socketUrl: "wss://wss-room-jp.queengame.io",
-        serverUrl: "wss://wss-hall.queengame.io",
-        open: 0,
         area: 1018,
+        open: 0,
+        point: "3_00",
+        id: 1003,
         name: 1018,
+        socketUrl: "wss://wss-room-jp.queengame.io",
         url: "https://wss-room-jp.queengame.io/ping",
-        type: 1,
-        point: "3_00"
+        type: 1
     }
 ];
