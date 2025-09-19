fceeea0bf0e5d502f3619e4707e9fc72d7fb9ab7
hjy <545925654@qq.com>
2025-09-19 18:42:59 +0800

    11111

--- snake/js/Config-dce6c.js
+++ snake/js/Config-dce6c.js
@@ -4,9 +4,9 @@
  * @Author: hjy
  * @Date: 2025-04-22 14:02:12
  * @LastEditors: hjy
- * @LastEditTime: 2025-09-19 17:15:37
+ * @LastEditTime: 2025-09-19 18:07:36
  */
-var game_version = "Version 0.1.0919.05";
+var game_version = "Version 0.1.0919.06";
 
 //Buffer
 // var Buffer_1 = require("buffer");
