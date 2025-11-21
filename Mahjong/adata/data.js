150ba1299d272a0913b6ac98aaa4600fbd78b963
17681268574 <490047678@qq.com>
2025-11-21 19:19:50 +0800

    麻将文件夹改名

--- Mahjong/adata/data.js
+++ Mahjong/adata/data.js
@@ -6,17 +6,19 @@
  * @LastEditors: hjy
  * @LastEditTime: 2025-11-20 19:03:01
  */
-window.v_type = 0; //0=QA；1=S0; 2=SN;
+window.v_type = 1; //0=QA；1=S0; 2=SN;
 window.gameId = 100005; //100001=贪吃蛇 100002=消消乐 100003=俄罗斯方块 100004=掼蛋 100005=麻将
 
 //服务器path
 window.server2 = "https://sso.cryptoarena.io"; //正式环境
 window.wsServer2 = "wss://wss-hall-1.cryptoarena.io"; //正式环境
-// window.address2 = "https://snake.cryptoarena.io; //外网-sn-web地址 (线上正式)
+// window.address2 = "https://guandan.cryptoarena.io; //外网-sn-web地址 (线上正式)
+// window.address2 = "https://mahjong.cryptoarena.io; //外网-sn-web地址 (线上正式)
 
 window.server1 = "https://sso.candyhouse.vip"; //测试环境
 window.wsServer1 = "wss://wss-hall-1.candyhouse.vip"; //测试环境
-// window.address1 = "https://snake.candyhouse.vip"; //外网-s0-web地址 (线上测试)
+// window.address1 = "https://guandan.candyhouse.vip"; //外网-s0-web地址 (线上测试)
+// window.address1 = "https://mahjong.candyhouse.vip"; //外网-s0-web地址 (线上测试)
 
 window.server0 = "http://13.250.6.10"; //开发环境
 window.wsServer0 = "ws://13.250.6.10:7055"; //开发服-qa-徐服务器
--- mahjong1/buffer-polyfill.js
+++
@@ -1,7 +0,0 @@
-// 确保Buffer在全局可用
-var global = global || window;
-var Buffer = Buffer || [];
-if (typeof window !== 'undefined') {
-    window.Buffer = window.Buffer || [];
-    window.global = window.global || window;
-} 
