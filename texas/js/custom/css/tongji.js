20c79765fdd693573b89108d49cee0c9d5a408b4
17681268574 <490047678@qq.com>
2026-04-20 14:30:36 +0800

    发布测试版改动

--- texas/js/custom/css/tongji.js
+++ texas/js/custom/css/tongji.js
@@ -1,13 +1,4 @@
-/*
- * @Descripttion:
- * @version:
- * @Author: hjy
- * @Date: 2024-04-09 13:06:24
- * @LastEditors: hjy
- * @LastEditTime: 2025-05-29 11:28:57
- * @Description:
- */
-var _hmt = _hmt || [];
+﻿var _hmt = _hmt || [];
 (function () {
     var hm = document.createElement("script");
     hm.src = "https://hm.baidu.com/hm.js?2f2e17b82f91b88a0a99db7ccba4eca4";
